c02d2a2b916ac3ca7c581d7cecf2e7bdd2b5c59d
eugenek45 <eugenekiprotich0@gmail.com>
2020-07-27 00:18:33 +0300

    created files

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$('#image').on("click",function(e){
+    $('.span').toggle('slow');
+    $('.imagE').toggle("click");
+   });
+
+
+$('#image1').on("click",function(e){
+    $('.span1').toggle('slow');
+    $('.images1').toggle("click");
+});
+
+
+$('#image2').on("click",function(e){
+    $('.span2').toggle('slow');
+    $('.images2').toggle("click");
+});
+
+   
