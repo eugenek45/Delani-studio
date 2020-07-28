a774266f82b044ff038974b90c422207259d4f06
eugenek45 <eugenekiprotich0@gmail.com>
2020-07-28 23:20:48 +0300

    added styling

--- js/index.js
+++ js/index.js
@@ -1,35 +1,89 @@
-$('#image').on("click",function(e){
-    $('.span').toggle('slow');
-    $('.imagE').toggle("click");
-   });
+//what we do section
+$(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown().hide()
+      $("#design").show();
+    });
+    $("#design").click(function(){
+      $("#design").slideUp();
+      $("#design-image").slideDown();
+    });
+     $("#development-image").click(function(){
+      $("#development-image").slideDown().hide()
+      $("#development").show();
+    });
+    $("#development").click(function(){
+      $("#development").slideUp();
+      $("#development-image").slideDown();
+    });
+      $("#product-image").click(function(){
+      $("#product-image").slideDown().hide()
+      $("#product").show();
+    });
+    $("#product").click(function(){
+      $("#product").slideUp();
+      $("#product-image").slideDown();
+    });
+  });
 
-
-$('#image1').on("click",function(e){
-    $('.span1').toggle('slow');
-    $('.images1').toggle("click");
+// poertfolio text overlay images
+$(document).ready(function(){
+  $("#work1").mouseover(function(){
+    $("#overlay").show();
+  }).mouseout(function(){
+    $("#overlay").hide();
+  });
+  $("#work2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+  $("#work3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+  $("#work4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+  $("#work5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+  $("#work6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+  $("#work7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+  $("#work8").mouseover(function(){
+    $("#overlay8").show();
+  }).mouseout(function(){
+    $("#overlay8").hide();
+  });
 });
 
-
-$('#image2').on("click",function(e){
-    $('.span2').toggle('slow');
-    $('.images2').toggle("click");
-});
-
-   
-  $(function () {
-    $("form#form").on('submit',function(event){
-       event.preventDefault();
-       let name = $("input#name").val();
-      
-       if ($("input#name").val()){
-           alert ("Hey " + name + ", Thanks for reaching out to us,Will get back to you");
-       }
-       else {
-           alert("Please provide your correct name and email!");
-       }
-
-   });
-
-
+//form input validation
+$(document).ready(function(){
+  $("form#formValidity").submit(function(event){
+    // event.preventDefault();
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+    if (name && email){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+    
+  });
 
 });
